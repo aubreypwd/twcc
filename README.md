db67229273098df045d58ce7641d7288045037f7
Aubrey Portwood <code@aubreypwd.com>
2020-06-25 12:55:46 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Usage
+
+```
+twcc examine|update|commands <left file> <middle file> <right file>
+```
+
+This command will compare `composer.json` packages in the middle file with those on the left and right and figure out which (of the left and right) have the greater version.
+
+### `examine`
+
+Will ouput suggested updates you need to make to the middle file to update to the latest version of the package it found to be greater in the left or right.
+
+### `update`
+
+Will write it's findings (the best version between the left and right file) to the middle file.
+
+### `commands`
+
+Will give you composer commands to run to update packages to the best versions found between the right and the left files.
---
+++ script.php
@@ -0,0 +1,93 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Composer\Semver\Comparator;
+
+$subcommand = $argv[1] ?? '';
+
+$subcommands = [
+	'examine',
+	'commands',
+	'update',
+];
+
+
+if ( ! in_array( $subcommand, $subcommands ) ) {
+	echo "Please use: twcc " . implode( '|', $subcommands ) . " [left file] [middle file] [right file]\n";
+	exit;
+}
+
+$files = [
+	'left'   => $argv[2] ?? '',
+	'middle' => $argv[3] ?? '',
+	'right'  => $argv[4] ?? '',
+];
+
+foreach ( $files as $file ) {
+	if ( ! file_exists( $file ) ) {
+		echo "The file '{$file}' does not exist.\n";
+		exit;
+	}
+}
+
+$json_files = [
+	'left'   => json_decode( file_get_contents( $files['left'] ), JSON_OBJECT_AS_ARRAY ),
+	'middle' => json_decode( file_get_contents( $files['middle'] ), JSON_OBJECT_AS_ARRAY ),
+	'right'  => json_decode( file_get_contents( $files['right'] ), JSON_OBJECT_AS_ARRAY ),
+];
+
+if ( ! is_array( $json_files['middle'] ) ) {
+	echo "The {$files[$key]} does not appear to be JSON.\n";
+}
+
+foreach ( [ 'require', 'require-dev'] as $require ) {
+	if ( ! isset( $json_files['middle'][ $require ] ) ) {
+		continue;
+	}
+
+	foreach ( $json_files['middle'][ $require ] as $package => $middle_version ) {
+		$left_version = $json_files['left'][ $require ][ $package ] ?? 'none';
+		$right_version = $json_files['right'][ $require ][ $package ] ?? 'none';
+
+		$middle_version_orig = $middle_version;
+
+		$version = '';
+
+		$updated = '';
+
+		if ( Comparator::greaterThan( $left_version, $middle_version ) ) {
+			$middle_version = $left_version;
+			$updated = $files['left'];
+		}
+
+		if ( Comparator::greaterThan( $right_version, $middle_version ) ) {
+			$middle_version = $right_version;
+			$updated = $files['right'];
+		}
+
+		$update = "updating {$package} from {$middle_version_orig} to {$middle_version} from {$updated}";
+
+		if ( empty( $updated ) ) {
+			continue;
+		}
+
+		if ( 'commands' === $subcommand ) {
+			$flag = 'require-dev' === $require ? '--save-dev' : '';
+
+			echo "composer update {$package}:{$middle_version} {$flag}\n";
+			continue;
+		}
+
+		if ( 'update' === $subcommand ) {
+			echo "Updating {$update}\n";
+			continue;
+		}
+
+		echo "Suggest {$update}\n";
+	}
+}
+
+if ( 'update' === $subcommand ) {
+	file_put_contents( "{$files['middle']}.new.json", json_encode( $json_files['middle'], JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES ) );
+}
